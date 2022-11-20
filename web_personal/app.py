771ed25ea86e6dfb5c3f79d08ca591d6e6f76541
Arturo Sanchez <thegamerslol2.zw@gmail.com>
2022-11-19 20:36:12 -0600

    Se agrega la logica de potfolio con for

--- web_personal/app.py
+++ web_personal/app.py
@@ -16,7 +16,22 @@ def contact():
 
 @app.route('/portfolio')
 def portfolio():
-    return render_template('public/portfolio.html')
+    projects=[#creamos el direccionario para el portafolio son sus caracteristicas 
+        {
+            'name':'Primer proyecto',
+            'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',
+            'image':'img/home-bg.jpg',
+            'url':'http://google.com'
+        },
+        {
+            'name':'Segundo proyecto',
+            'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',
+            'image':'img/about-bg.jpg',
+            'url':'http://google.com'
+        }
+    ]
+    return render_template('public/portfolio.html', projects=projects)
+
 
 ######### Auth ###########
 @app.route('/auth/login')
